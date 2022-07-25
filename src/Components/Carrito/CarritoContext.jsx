fea50c6e80c20731ef6ad020d0c0091934f76861
Agustin-FE <agustinporta2000@gmail.com>
2022-07-25 05:04:18 -0300

    terminado

--- src/Components/Carrito/CarritoContext.jsx
+++ src/Components/Carrito/CarritoContext.jsx
@@ -25,22 +25,6 @@ function CartProvider( {children}) {
     const addItemToCart = (product, cant) => {
 
         setCartItems([...cartItems, {...product, amount: cant}])
-
-        /* const inCart = cartItems.find((productInCart) => productInCart.id === product.id)
-
-        if (inCart) {
-            setCartItems(
-                cartItems.map((productInCart) => {
-                    if (productInCart.id === product.id) {
-                        return { ...inCart, amount: inCart.amount + 1 }
-                    }
-                    else return productInCart;
-                })
-            )
-        }
-        else {
-            setCartItems([...cartItems, { ...product, amount: 1 }])
-        } */
     }
 
     const deleteItemToCart = (product) => {
@@ -53,45 +37,6 @@ function CartProvider( {children}) {
             inCart
         )  
     }
-
-    /* const addItemToCart = (product) => {
-        const inCart = cartItems.find((productInCart) => productInCart.id === product.id)
-
-        if (inCart) {
-            setCartItems(
-                cartItems.map((productInCart) => {
-                    if (productInCart.id === product.id) {
-                        return { ...inCart, amount: inCart.amount + 1 }
-                    }
-                    else return productInCart;
-                })
-            )
-        }
-        else {
-            setCartItems([...cartItems, { ...product, amount: 1 }])
-        }
-    }
-
-    const deleteItemToCart = (product) => {
-        const inCart = cartItems.find(
-            (productInCart) => productInCart.id === product.id  
-        );
-
-        if (inCart.amount === 1) {
-            setCartItems(
-                cartItems.filter(productInCart => productInCart.id !== product.id)
-            )
-        } else {
-            setCartItems((productInCart) => {
-                if (productInCart.id === product.id) {
-                    return { ...inCart, amount: inCart.amount - 1 }
-                }
-                else return productInCart;
-            })
-        }
-    } */
-
-
 
     return(
         <CartContext.Provider value={{cartItems, addItemToCart, deleteItemToCart}}>
